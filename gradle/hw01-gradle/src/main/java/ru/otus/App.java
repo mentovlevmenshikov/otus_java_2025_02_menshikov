0a37a56d50ca3de42d345d04d238a4db3836e77a
Alexey <mentovlev@gmail.com>
2025-03-08 04:05:35 +0500

    init: home work 1

---
+++ gradle/hw01-gradle/src/main/java/ru/otus/App.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var guava = new HelloOtus();
+        guava.callGuava();
+    }
+}
